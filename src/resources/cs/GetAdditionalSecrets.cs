111fc140c2c3485d7376cb5b8427e13bd943d8cf
petrak@ <petrathekat@gmail.com>
2023-08-23 22:35:40 -0500

    make villages who love or hate sultans tell you about the sultans

--- src/resources/cs/GetAdditionalSecrets.cs
+++ src/resources/cs/GetAdditionalSecrets.cs
@@ -29,30 +29,13 @@ namespace at.petrak.whereiseverybody {
       if (village != null) {
         // then they are primarily part of a village
         UnityEngine.Debug.Log("live in a village");
-        var polarizingCreatures = new List<string>();
-        if (village.properties.TryGetValue("worships_creature_id", out string worship)) {
-          polarizingCreatures.Add(worship);
-        }
-        if (village.properties.TryGetValue("despises_creature_id", out string despise)) {
-          polarizingCreatures.Add(despise);
-        }
-
-        if (polarizingCreatures.Count > 0) {
-          UnityEngine.Debug.Log("> have village feelings about : " + string.Join(", ", polarizingCreatures));
-          foreach (var lair in worldInfo.lairs) {
-            if (polarizingCreatures.Contains(lair.ownerID)) {
-              var note = JournalAPI.GetMapNote(lair.secretID);
-              if (note != null && !note.revealed) {
-                res.Add(note);
-              }
-            }
-          }
-        }
+        villageLegendaryCreatures(village, res);
+        villageSultan(village, res);
       }
 
       UnityEngine.Debug.Log("Returning:");
       foreach (var secret in res) {
-        UnityEngine.Debug.Log("  - " + secret.GetDisplayText());
+        UnityEngine.Debug.Log("  - " + secret.GetShortText());
       }
       return res;
     }
@@ -70,9 +53,97 @@ namespace at.petrak.whereiseverybody {
 
       UnityEngine.Debug.Log("found village object " + villageObj.secretID);
       var note = JournalAPI.GetMapNote(villageObj.secretID);
-      UnityEngine.Debug.Log("found note object " + note?.GetDisplayText());
+      UnityEngine.Debug.Log("found note object " + note?.GetShortText());
       if (note == null || note.revealed) return null;
       return note;
+    }
+
+    // Get the locations of creatures the village reveres or hates
+    private static void villageLegendaryCreatures(HistoricEntitySnapshot village, List<IBaseJournalEntry> mutOut) {
+      var polarizingCreatures = new List<string>();
+      if (village.properties.TryGetValue("worships_creature_id", out string worship)) {
+        polarizingCreatures.Add(worship);
+      }
+      if (village.properties.TryGetValue("despises_creature_id", out string despise)) {
+        polarizingCreatures.Add(despise);
+      }
+
+      if (polarizingCreatures.Count > 0) {
+        UnityEngine.Debug.Log("> have village feelings about : " + string.Join(", ", polarizingCreatures));
+        foreach (var lair in worldInfo.lairs) {
+          if (polarizingCreatures.Contains(lair.ownerID)) {
+            var note = JournalAPI.GetMapNote(lair.secretID);
+            if (note != null && !note.revealed) {
+              mutOut.Add(note);
+            }
+          }
+        }
+      }      
+    }
+
+    // Get secrets of sultans the village loves or hates
+    private static void villageSultan(HistoricEntitySnapshot village, List<IBaseJournalEntry> mutOut) {
+      // Add secrets about sultans they love
+      var polarizingSultans = new List<(string, bool)>();
+      if (village.properties.TryGetValue("worships_sultan_id", out string worship)) {
+        polarizingSultans.Add((worship, true));
+      }
+      if (village.properties.TryGetValue("despises_sultan_id", out string despise)) {
+        polarizingSultans.Add((despise, false));
+      }
+
+      foreach (var (sultan, wantGood) in polarizingSultans) {
+        UnityEngine.Debug.Log("here is my opinions of the events for " + sultan);
+        var notes = JournalAPI.GetNotesForSultan(sultan);
+        foreach (var note in notes) {
+          var evt = The.Game.sultanHistory.GetEvent(note.eventId);
+          var kind = evt.getEventProperty("tombInscriptionCategory");
+          var isGood = isSultanEventGood(kind);
+          UnityEngine.Debug.Log(string.Format("- {0} : {1}", kind, isGood));
+          if (!note.revealed) {
+            if (isGood == null || isGood == wantGood) {
+              mutOut.Add(note);
+            }
+          }
+        }
+      }
+    }
+
+    // null = good and bad
+    private static bool? isSultanEventGood(string kind) {
+      switch(kind) {
+        case "Dies":
+        case "BodyExperienceBad":
+        case "CommitsFolly":
+        case "EnduresHardship":
+        case "DoesBureaucracy":
+        case "DoesSomethingDestructive":
+          return false;
+
+        case "IsBorn":
+        case "DoesSomethingRad": // real code written by real developers
+        case "WieldsItemInBattle":
+        case "Trysts":
+        case "CreatesSomething":
+        case "LearnsSecret":
+        case "Treats":
+        case "HasInspiringExperience":
+        case "Slays":
+        case "CrownedSultan":
+        case "MeetsWithCounselors":
+          return true;
+
+        case "DoesSomethingHumble":
+        // This is the type both for freeing AND sacking a city, which I can't distinguish.
+        // waiting on the next FF
+        case "Resists":
+          return null;
+        
+        default: // give them the benefit of the doubt if i missed something
+          UnityEngine.Debug.Log("whoops! i forgot to write if `" + kind + "` was a good or bad thing");
+          return true;
+
+      }
     }
   }
 }
--- src/resources/cs/Wishes.cs
+++ src/resources/cs/Wishes.cs
@@ -1,6 +1,7 @@
 using XRL;
 using XRL.Wish;
 using XRL.World.WorldBuilders;
+using Qud.API;
 
 namespace at.petrak.whereiseverybody {
   [HasWishCommand]
@@ -20,6 +21,27 @@ namespace at.petrak.whereiseverybody {
       UnityEngine.Debug.Log("all the lairs:");
       foreach (var lair in worldInfo.lairs) {
           UnityEngine.Debug.Log("  - name: " + lair.name + ", owner id: " + lair.ownerID + ", secret id: " + lair.secretID);
+      }
+    }
+
+    [WishCommand(Command = "pk_sultans")]
+    public static void PrintSultans() {
+      UnityEngine.Debug.Log("all the sultans:");
+      foreach (var sultan in HistoryAPI.GetSultans()) {
+        UnityEngine.Debug.Log("  - " + sultan.id);
+      }
+    }
+    
+    [WishCommand(Command = "pk_villageprops")]
+    public static void PrintVillageProps(string rest) {
+      UnityEngine.Debug.Log("properties for " + rest);
+      var village = HistoryAPI.GetVillageSnapshot(rest);
+      if (village == null) {
+        UnityEngine.Debug.Log("no village found");
+      } else {
+        foreach (var pair in village.properties) {
+          UnityEngine.Debug.Log("  - " + pair.Key + " : " + pair.Value);
+        }
       }
     }
   }
